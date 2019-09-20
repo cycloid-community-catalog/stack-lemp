06b1a4104a437673a9d4dd7035b596b89743104b
talset <talset55@gmail.com>
2019-09-20 11:37:11 +0200

    terraform: be able to use short_name for resource
    help to avoid :
    
    Error: "name" cannot be longer than 32 characters

--- terraform/module-lemp/variables.tf
+++ terraform/module-lemp/variables.tf
@@ -276,14 +276,20 @@ variable "elasticache_cluster_id" {
   default = ""
 }
 
-resource "random_string" "elasticache_cluster_id" {
+variable "default_short_name" {
+  default = ""
+}
+
+resource "random_string" "id" {
   length  = 18
   upper   = false
   special = false
 }
 
+#local.default_short_name is lenght 20
 locals {
-  elasticache_cluster_id = var.elasticache_cluster_id != "" ? var.elasticache_cluster_id : "cy${random_string.elasticache_cluster_id.result}"
+  elasticache_cluster_id = var.elasticache_cluster_id != "" ? var.elasticache_cluster_id : "cy${random_string.id.result}"
+  default_short_name     = var.default_short_name != "" ? var.default_short_name : "cy${random_string.id.result}"
 }
 
 variable "deploy_bucket_name" {
